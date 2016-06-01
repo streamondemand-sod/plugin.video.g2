e9094f12c81187aea74a2d46386990bf97cd37ad
J0rdy Z <jrdy.z65@gmail.com>
2016-06-01 13:17:23 +0000

    Fixed importing of modules declared as required in the addon.xml - Now we can support script.module.requests and others - Added ability to debug logging only for specific modules

--- lib/importer.py
+++ lib/importer.py
@@ -25,45 +25,27 @@ import pkgutil
 import xbmc
 
 
-#                           === IMPORTANT NOTE ===
-# For some reason, declaring an addon required in the g2 addon.xml seems to broke
-# the importer in way that the addon itself cannot be directly imported and used.
-#
-# For example:
-#   [plugin.video.g2/addon.xml]
-#           <requires>
-#               ...
-#               <import addon="script.module.metahandler" optional="true" />
-#           </requires>
-#
-# Actually prevents to use metahandler in the g2 code, like:
-#   from metahandler import metahandlers
-#   metahandlers.MetaDat()...
-#
-# It chokes on the import statement with an error like:
-#   ImportError: No module named TMDB
-#
-# On the other hand, creating a package for exactly the same module and let it be
-# loaded through the packages methods, works fine!
-#
-# (fixme)[code]: direct import of required addon libraries
-#
+_LOG_LEVEL = False # xbmc.LOGNOTICE # set different from None to get logging in kodi
+_LOG_MODULE = '' # 'script.module.requests' # if specified will log only imports under this sandboxed path
 
-
-_log_level = False # xbmc.LOGNOTICE
-_handled_paths = {}
+_HANDLED_PATHS = {}
 _SANDBOX_NAMESPACE_SEP = '@'
+
+
+def _log(sandbox, msg):
+    if _LOG_LEVEL and (not _LOG_MODULE or sandbox == _normalize_sandbox_name(_LOG_MODULE)):
+        xbmc.log('[%s%s] %s'%(sys.argv[0], '' if len(sys.argv) <= 1 else ':'+sys.argv[1], msg), level=_LOG_LEVEL)
 
 
 def _name_is_sandboxed(fullname):
     return _SANDBOX_NAMESPACE_SEP in fullname
 
 
-def _name_sandbox(self, fullname):
-    if _name_is_sandboxed(fullname) or not self.sandbox:
+def _name_sandbox(sandbox, fullname):
+    if _name_is_sandboxed(fullname) or not sandbox:
         return fullname
     else:
-        return self.sandbox+_SANDBOX_NAMESPACE_SEP+fullname
+        return sandbox+_SANDBOX_NAMESPACE_SEP+fullname
 
 
 def _name_get_sandbox(fullname):
@@ -74,25 +56,22 @@ def _name_desandbox(fullname):
     return fullname if not _name_is_sandboxed(fullname) else fullname.split(_SANDBOX_NAMESPACE_SEP, 1)[1]
 
 
-def log(msg):
-    if _log_level: xbmc.log(msg, level=_log_level)
+def _normalize_sandbox_name(name):
+    return name.replace('.', '_')
 
 
 def add_path(path):
-    if not os.path.isdir(path): return
-    sandbox = os.path.basename(path).replace('.', '_')
-    for p in [path, os.path.realpath(path)]:
-        if p not in _handled_paths:
-            _handled_paths[p] = sandbox
-            log('importer.add_path(%s): added path %s with %s as sandbox prefix'%(path, p, sandbox))
+    if not os.path.isdir(path):
+        return
+    sandbox = _normalize_sandbox_name(os.path.basename(path))
+    for path in [path, os.path.realpath(path)]:
+        if path not in _HANDLED_PATHS:
+            _HANDLED_PATHS[path] = sandbox
+            _log(sandbox, 'importer: imports under path %s handled as %s'%(path, sandbox))
 
 
 def walk_packages(path=None, prefix='', onerror=None):
-    log('importer.walk_packages(%s, %s, %s)'%(path, prefix, onerror))
-
     for package, name, is_pkg in pkgutil.walk_packages(path, prefix, onerror):
-        log('importer.walk_packages(%s, ...): yield: %s, %s, %s'%(path, package, _name_desandbox(name), is_pkg))
-
         yield package, _name_desandbox(name), is_pkg
 
 
@@ -114,84 +93,101 @@ class ImpImporterSandbox(pkgutil.ImpImporter):
         NOTE2: the ImpImporterSandbox doesn't respect the module reload semantic.
     """
     def __init__(self, path_entry):
-        log('ImpImporterSandbox.__init__(%s, %s)'%(self, path_entry))
-
-        for hp, sandbox in _handled_paths.iteritems():
-            if path_entry.startswith(hp):
+        self.sandbox = None
+        for hpath, sandbox in _HANDLED_PATHS.iteritems():
+            if path_entry.startswith(hpath):
                 pkgutil.ImpImporter.__init__(self, path_entry)
 
                 self.sandbox = sandbox
-                log('ImpImporterSandbox.__init__(..., %s): handled: sandbox=%s'%(path_entry, self.sandbox))
+                self._log('ImpImporterSandbox(%s, %s): handled: sandbox=%s'%(self, path_entry, self.sandbox))
                 return
 
-        log('ImpImporterSandbox.__init__(..., %s): not handled'%path_entry)
+        self._log('ImpImporterSandbox(%s, %s): not handled'%(self, path_entry))
         raise ImportError()
 
     def find_module(self, fullname, path=None):
-        log('ImpImporterSandbox.find_module(%s, %s, %s)'%(self, fullname, path))
+        self._log('find_module(%s, %s, %s)'%(self, fullname, path))
 
         if self.sandbox is None:
-            log('ImpImporterSandbox.find_module(): sandbox is None')
+            self._log('find_module(): sandbox is None')
             return None
 
         impl = pkgutil.ImpImporter.find_module(self, fullname, path)
         if not impl:
-            log('ImpImporterSandbox.find_module(..., %s, ...): module not found'%fullname)
+            self._log('find_module(..., %s, ...): module not found'%fullname)
             return None
 
-        log('ImpImporterSandbox.find_module(..., %s, ...): sandbox=%s, impl.fullname=%s, file=%s, filename=%s, etc=%s'%(fullname, self.sandbox, impl.fullname, impl.file, impl.filename, impl.etc))
+        self._log('find_module(..., %s, ...): sandbox=%s, impl.fullname=%s, file=%s, filename=%s, etc=%s'%
+                  (fullname, self.sandbox, impl.fullname, impl.file, impl.filename, impl.etc))
 
-        return ImpLoaderSandbox(_name_sandbox(self, impl.fullname), impl.file, impl.filename, impl.etc)
+        return ImpLoaderSandbox(_name_sandbox(self.sandbox, impl.fullname), impl.file, impl.filename, impl.etc)
 
     def iter_modules(self, prefix=''):
-        log('ImpImporterSandbox.iter_modules(%s, %s)'%(self, prefix))
+        self._log('iter_modules(%s, %s)'%(self, prefix))
 
         for name, is_pkg in pkgutil.ImpImporter.iter_modules(self, prefix):
-            log('ImpImporterSandbox.iter_modules(..., %s): yield: %s, %s'%(prefix, _name_sandbox(self, name), is_pkg))
+            self._log('iter_modules(..., %s): yield: %s, %s'%(prefix, _name_sandbox(self.sandbox, name), is_pkg))
 
-            yield _name_sandbox(self, name), is_pkg
+            yield _name_sandbox(self.sandbox, name), is_pkg
+
+    def _log(self, msg):
+        _log(self.sandbox, msg)
 
 
 class ImpLoaderSandbox(pkgutil.ImpLoader):
     def __init__(self, fullname, file, filename, etc):
-        log('ImpLoaderSandbox.__init__(%s, %s, %s, %s, %s)'%(self, fullname, file, filename, etc))
-
         self.sandbox = _name_get_sandbox(fullname)
+
+        self._log('ImpLoaderSandbox(%s, %s, %s, %s, %s)'%(self, fullname, file, filename, etc))
 
         pkgutil.ImpLoader.__init__(self, fullname, file, filename, etc)
 
     def load_module(self, fullname):
-        log('ImpLoaderSandbox.load_module(%s, %s)'%(self, fullname))
+        self._log('load_module(%s, %s)'%(self, fullname))
 
-        sandboxfullname = _name_sandbox(self, fullname)
+        sandboxfullname = _name_sandbox(self.sandbox, fullname)
 
-        # Load all parents modules first
-        parents = _name_desandbox(sandboxfullname).split('.')
-        for i in range(0, len(parents)-1):
-            sandboxparent = _name_sandbox(self, '.'.join(parents[0:i+1]))
-            if sandboxparent in sys.modules:
-                log('ImpLoaderSandbox.load_module(..., %s): parent module %s found in the cache'%(fullname, sandboxparent))
-            else:
-                log('ImpLoaderSandbox.load_module(..., %s): loading parent module %s...'%(self, sandboxparent))
-
-                if not pkgutil.ImpLoader.load_module(self, sandboxparent):
-                    log('ImpLoaderSandbox.load_module(..., %s): loading parent module %s failed'%(self, sandboxparent))
+        # NOTE: Kodi, before running an addon, seems to load the top level packages found in the
+        #   required section of the addon itself (e.g. for the dependency script.module.requests,
+        #   Kodi loads the "requests" package found in "addons/script.module.requests/lib/" directory).
+        #   This is done before the importer machinery is hooked up by the addon, so the required
+        #   top level packages are inserted in sys.modules not sandboxed (e.g. as "requests" not as
+        #   "script_module_requests@requests"). This is not a problem from a conflict point of
+        #   view as these packages should not conflict at all and, if they are, the importer module
+        #   can change the import statement. However, subsequently, this impoter fails to find them.
+        #   The check below is performed to verify this scenario and eventually patch the sys.modules.
+        cleanfullname = _name_desandbox(sandboxfullname)
+        grandparent = cleanfullname.split('.')[0]
+        if grandparent != cleanfullname:
+            sandboxgrandparent = _name_sandbox(self.sandbox, grandparent)
+            if sandboxgrandparent in sys.modules:
+                self._log('load_module(..., %s): sandboxed grand parent module %s found in the cache: %s'%
+                          (fullname, sandboxgrandparent, sys.modules[sandboxgrandparent]))
+            elif grandparent in sys.modules:
+                self._log('load_module(..., %s): grand parent module %s found in the cache: %s'%
+                          (fullname, grandparent, sys.modules[grandparent]))
+                # The top level module is already loaded in sys.modules not sandboxed
+                sys.modules[sandboxgrandparent] = sys.modules[grandparent]
 
         # NOTE: This breaks the module reload semantic because of the sys.modules check!
-        # NOTE: Hopefully, not many addons are going to use the reload mechanism.
+        #   Hopefully, not many addons are going to use the reload mechanism.
         if sandboxfullname in sys.modules:
-            log('ImpLoaderSandbox.load_module(..., %s): module %s found in the cache'%(fullname, sandboxfullname))
-
             mod = sys.modules[sandboxfullname]
+
+            self._log('load_module(..., %s): module %s found in the cache: %s'%(fullname, sandboxfullname, mod))
         else:
-            log('ImpLoaderSandbox.load_module(..., %s): loading module %s...'%(self, sandboxfullname))
+            self._log('load_module(..., %s): loading module %s...'%(self, sandboxfullname))
 
             mod = pkgutil.ImpLoader.load_module(self, sandboxfullname)
 
         if not mod:
-            log('ImpLoaderSandbox.load_module(..., %s): loading module %s failed'%(fullname, sandboxfullname))
+            self._log('load_module(..., %s): loading module %s failed'%(fullname, sandboxfullname))
         else:
-            log('ImpLoaderSandbox.load_module(..., %s): module %s loaded: __name__=%s, __file__=%s, __package__=%s, __path__=%s'%
-                (fullname, sandboxfullname, mod.__name__, mod.__file__, mod.__package__, mod.__path__ if hasattr(mod, '__path__') else 'NO ATTRIBUTE'))
+            self._log('load_module(..., %s): %s loaded: __name__=%s, __file__=%s, __package__=%s, __path__=%s'%
+                      (fullname, sandboxfullname, mod.__name__, mod.__file__, mod.__package__,
+                       mod.__path__ if hasattr(mod, '__path__') else 'NO ATTRIBUTE'))
 
         return mod
+
+    def _log(self, msg):
+        _log(self.sandbox, msg)
